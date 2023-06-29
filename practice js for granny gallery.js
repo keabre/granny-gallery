531b8e3e5355ab3cb95fed7e0f55536e8efb0ba6
keabre <kieran1907kr@gmail.com>
2023-06-29 15:00:50 +0100

    added Quantity to gallery and started adding ID's for JS, also added new js file and script element in HTML for gallery

---
+++ practice js for granny gallery.js
@@ -0,0 +1,64 @@
+/* 
+functions of the website:
+track what user chooses to but and calculates final costs (pieces + postage and packaging)
+tells user if paintings are available and their amounts
+
+-set variable for every painting? in place for a database? or use SQL?
+-calculation function that takes user inputs and adds up into a final cost for piece variable and a postage and packaging variable
+
+assume using variables for everything
+const for all artwork
+
+variable names for artwork:
+-let cockerelimg-price
+-let fruitimg-price
+-let redroofhouseimg-price
+-let thatchedroofhouseimg-price
+-let housewithwindowimg-price
+-let sleepingkittenimg-price
+-let paintedhorseimg-price
+-let sketchedhorseimg-price
+-let popartimg-price
+-let potteryimg-price
+-let mouseonbikeimg-price
+-let mousewithglassesimg-price
+-let ladiesinredimg-price
+-let twowhitehensimg-price
+-let treebylakeimg-price
+-let floweryladyimg-price
+-let headbangladyimg-price
+
+*/
+
+let cockerelimg_price = 7.50;
+let fruitimg_price = 6.00;
+let redroofhouseimg_price = 3.00;
+let thatchedroofhouseimg_price = 3.00;
+let housewithwindowimg_price = 60.00;
+let sleepingkittenimg_price = 5.00;
+let paintedhorseimg_price = 10.00;
+let sketchedhorseimg_price = 8.00;
+let popartimg_price = 100.00;
+let potteryimg_price = 120.00;
+let mouseonbikeimg_price = 4.00;
+let mousewithglassesimg_price = 4.50;
+let ladiesinredimg_price = 90.00;
+let twowhitehensimg_price = 7.50;
+let treebylakeimg_price = 75.00;
+let floweryladyimg_price = 6.00;
+let headbangladyimg_price = 6.00;
+
+let cockerelimg_quant = 7.50;
+let fruitimg_quant = 6.00;
+let redroofhouseimg_quant = 3.00;
+let thatchedroofhouseimg_quant = 3.00;
+let sleepingkittenimg_quant = 5.00;
+let paintedhorseimg_quant = 10.00;
+let sketchedhorseimg_quant = 8.00;
+let mouseonbikeimg_quant = 4.00;
+let mousewithglassesimg_quant = 4.50;
+let twowhitehensimg_quant = 7.50;
+let floweryladyimg_quant = 6.00;
+let headbangladyimg_quant = 6.00;
+
+
