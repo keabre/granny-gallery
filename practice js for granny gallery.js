88d9d8e386e9e59bcd0bc8fbe14a1483177ada11
keabre <kieran1907kr@gmail.com>
2023-07-14 21:59:55 +0100

    completed creating code for quantity and price text

--- practice js for granny gallery.js
+++ practice js for granny gallery.js
@@ -36,37 +36,25 @@ variable names for artwork:
     collection of arrays in an object
 */
 
-const DatabaseObject = {
-    cockerel: ["cockerel", 7.50, "cockerel-price-text", ""],
-    fruit: ["fruit", 6.00, "fruit-price-text", ""],
-    redroofhouse: ["redroofhouse", 3.00, "redroofhouse-price-text", ""],
-    thatchedroofhouse: ["thatchedroofhouse", 3.00, "thatchedroofhouse-price-text", ""],
-    housewithwindow: ["housewithwindow", 60.00, "housewithwindow-price-text", ""],
-    sleepingkitten: ["sleepingkitten", 5.00, "sleepingkitten-price-text", ""],
-    paintedhorse: ["paintedhorse", 10.00, "paintedhorse-price-text", ""],
-    sketchedhorse: ["sketchedhorse", 8.00, "sketchedhorse-price-text", ""],
-    popart: ["popart", 100.00, "popart-price-text", ""],
-    pottery: ["pottery", 120.00, "pottery-price-text", ""],
-    mouseonbike: ["mouseonbike", 4.00, "mouseonbike-price-text", ""],
-    mousewithglasses: ["mousewithglasses", 4.50, "mousewithglasses-price-text", ""],
-    ladiesinred: ["ladiesinred", 90.00, "ladiesinred-price-text", ""],
-    twowhitehens: ["twowhitehens", 7.50, "twowhitehens-price-text", ""],
-    treebylake: ["treebylake", 75.00, "treebylake-price-text", ""],
-    flowerylady: ["flowerylady", 6.00, "flowerylady-price-text", ""],
-    headbandlady: ["headbandlady", 6.00, "headbandlady-price-text", ""]
+let DatabaseObject = {
+    cockerel: ["cockerel", 7.50, "cockerel-price-text", "print", 10, "cockerel-quant-text"],
+    fruit: ["fruit", 6.00, "fruit-price-text", "print", 10, "fruit-quant-text"],
+    redroofhouse: ["redroofhouse", 3.00, "redroofhouse-price-text", "greetings card", 10, "redroofhouse-quant-text"],
+    thatchedroofhouse: ["thatchedroofhouse", 3.00, "thatchedroofhouse-price-text", "greetings card", 10, "thatchedroofhouse-quant-text"],
+    housewithwindow: ["housewithwindow", 60.00, "housewithwindow-price-text", "original", 1, "housewithwindow-quant-text"],
+    sleepingkitten: ["sleepingkitten", 5.00, "sleepingkitten-price-text", "print", 10, "sleepingkitten-quant-text"],
+    paintedhorse: ["paintedhorse", 10.00, "paintedhorse-price-text", "print", 10, "paintedhorse-quant-text"],
+    sketchedhorse: ["sketchedhorse", 8.00, "sketchedhorse-price-text", "print", 10, "sketchedhorse-quant-text"], 
+    popart: ["popart", 100.00, "popart-price-text", "original", 1, "popart-quant-text"],
+    pottery: ["pottery", 120.00, "pottery-price-text", "original", 1, "pottery-quant-text"],
+    mouseonbike: ["mouseonbike", 4.00, "mouseonbike-price-text", "greetings card", 10, "mouseonbike-quant-text"],
+    mousewithglasses: ["mousewithglasses", 4.50, "mousewithglasses-price-text", "greetings card", 10, "mousewithglasses-quant-text"],
+    ladiesinred: ["ladiesinred", 90.00, "ladiesinred-price-text", "original", 1, "ladiesinred-quant-text"],
+    twowhitehens: ["twowhitehens", 7.50, "twowhitehens-price-text", "print", 10, "twowhitehens-quant-text"],
+    treebylake: ["treebylake", 75.00, "treebylake-price-text", "original", 1, "treebylake-quant-text"],
+    flowerylady: ["flowerylady", 6.00, "flowerylady-price-text", "print", 10, "flowerylady-quant-text"],
+    headbandlady: ["headbandlady", 6.00, "headbandlady-price-text", "print", 10, "headbandlady-quant-text"]
 }
-
-/* 
-    object for instances of objects for images and their respective information (name, price, id, type)
-
-    let ImgObject = {
-        this.imgname: imgname,
-        this.imgprice: imgprice,
-        this.imgid: imgid,
-        this.imgtype: imgtype
-
-    }
-*/
 
 /* 
     list of instances of objects for images 
@@ -88,143 +76,45 @@ const DatabaseObject = {
         };
         return obj;
     }
-
-
-
-
 */
 
 
-
-/* 
-
-    prospective function for js img price allocation
-
-    function AllocateImgPrices(nameofimg, imgprice, imgpricetextid) {
-        let `${nameofimg}+_img_price` = imgprice;
-        const `${nameofimg}+Price` = document.querySelector('#imgpricetextid');
-        `${nameofimg}+Price`.textContent += `${nameofimg}+_img_price`;
+function createImgObject(imgname, imgprice, imgpriceid, imgtype, imgquant, imgquantid) {
+    let obj = {};
+    obj.imgquant = imgquant;
+    obj.imgquantid = imgquantid;
+    obj.imgname = imgname;
+    obj.imgprice = imgprice;
+    obj.imgpriceid = imgpriceid;
+    obj.imgtype = imgtype;
+    obj.AllocateImgPrice = function () {
+        let imageprice = `${this.imgprice}`;
+        let SelectedImgPriceId;
+        let priceidtext = '#'.concat(this.imgpriceid);
+        SelectedImgPriceId = document.querySelector(priceidtext);
+        SelectedImgPriceId.textContent += imageprice;
+        
+    };
+    obj.AllocateImgQuant = function () {
+        let imagequant = `${this.imgquant}`;
+        let SelectedImgQuantId;
+        let quantidtext = '#'.concat(this.imgquantid);
+        SelectedImgQuantId = document.querySelector(quantidtext);
+        SelectedImgQuantId.textContent += imagequant;
     }
+    return obj;
+}
+let ImgObjectList = [];
 
-    this function is used alongside a list of objects
+for (let picture in DatabaseObject){
+    let object = createImgObject(DatabaseObject[picture][0], DatabaseObject[picture][1], DatabaseObject[picture][2], DatabaseObject[picture][3], DatabaseObject[picture][4], DatabaseObject[picture][5]);
+    ImgObjectList.push(object);
+}
 
-*/
+for (let i = 0; i < ImgObjectList.length; i++) {
+    ImgObjectList[i].AllocateImgPrice();
+    ImgObjectList[i].AllocateImgQuant();
+}
 
-let cockerelimg_price = 7.50;
-const cockerelImgPrice = document.querySelector('#cockerel-img-price');
-cockerelImgPrice.textContent += cockerelimg_price;
-
-let fruitimg_price = 6.00;
-const fruitImgPrice = document.querySelector('#fruit-img-price');
-fruitImgPrice.textContent += fruitimg_price;
-
-let redroofhouseimg_price = 3.00;
-const redRoofHouseImgPrice = document.querySelector('#house-with-point-img-price');
-redRoofHouseImgPrice.textContent += redroofhouseimg_price;
-
-let thatchedroofhouseimg_price = 3.00;
-const thatchedRoofHouseImgPrice = document.querySelector('#house-with-thatched-roof-img-price');
-thatchedRoofHouseImgPrice.textContent += thatchedroofhouseimg_price;
-
-let housewithwindowimg_price = 60.00;
-const houseWithWindowImgPrice = document.querySelector('#house-with-window-img-price');
-houseWithWindowImgPrice.textContent += housewithwindowimg_price;
-
-let sleepingkittenimg_price = 5.00;
-const sleepingKittenImgPrice = document.querySelector('#kitten-with-white-crop-img-price');
-sleepingKittenImgPrice.textContent += sleepingkittenimg_price;
-
-let paintedhorseimg_price = 10.00;
-const paintedHorseImgPrice = document.querySelector('#painted-horse-img-price');
-paintedHorseImgPrice.textContent += paintedhorseimg_price;
-
-let sketchedhorseimg_price = 8.00;
-const sketchedHorseImgPrice = document.querySelector('#pencil-drawn-horse-img-price');
-sketchedHorseImgPrice.textContent += sketchedhorseimg_price;
-
-let popartimg_price = 100.00;
-const popartImgPrice = document.querySelector('#popart-img-price');
-popartImgPrice.textContent += popartimg_price;
-
-let potteryimg_price = 120.00;
-const potteryImgPrice = document.querySelector('#pottery-img-price');
-potteryImgPrice.textContent += potteryimg_price;
-
-let mouseonbikeimg_price = 4.00;
-const mouseOnBikeImgPrice = document.querySelector('#rat-on-bike-img-price');
-mouseOnBikeImgPrice.textContent += mouseonbikeimg_price;
-
-let mousewithglassesimg_price = 4.50;
-const mouseWithGlassesImgPrice = document.querySelector('#rat-with-glasses-img-price')
-mouseWithGlassesImgPrice.textContent += mousewithglassesimg_price;
-
-let ladiesinredimg_price = 90.00;
-const ladiesInRedImgPrice = document.querySelector('#two-ladies-dressed-in-red-img-price')
-ladiesInRedImgPrice.textContent += ladiesinredimg_price;
-
-let twowhitehensimg_price = 7.50;
-const twoWhiteHensImgPrice = document.querySelector('#two-white-hens-img-price');
-twoWhiteHensImgPrice.textContent += twowhitehensimg_price;
-
-let treebylakeimg_price = 75.00;
-const treeByLakeImgPrice = document.querySelector('#whispy-tree-img-price');
-treeByLakeImgPrice.textContent += treebylakeimg_price;
-
-let floweryladyimg_price = 6.00;
-const floweryLadyImgPrice = document.querySelector('#woman-with-flowers-in-hair-img-price');
-floweryLadyImgPrice.textContent += floweryladyimg_price;
-
-let headbandladyimg_price = 6.00;
-const headbandLadyImgPrice = document.querySelector('#woman-with-headband-img-price');
-headbandLadyImgPrice.textContent += headbandladyimg_price;
-
-
-let jscockerelimg_quant = 10;
-let cockerelimg_quant = document.getElementById("cockerelimg_quant");
-cockerelimg_quant.textContent = jscockerelimg_quant;
-
-let jsfruitimg_quant = 10;
-let fruitimg_quant = document.getElementById("fruitimg_quant");
-fruitimg_quant.textContent = jsfruitimg_quant;
-
-let jsredroofhouseimg_quant = 10;
-let redroofhouseimg_quant = document.getElementById("redroofhouseimg_quant");
-redroofhouseimg_quant.textContent = jsredroofhouseimg_quant;
-
-let jsthatchedroofhouseimg_quant = 10;
-let thatchedroofhouseimg_quant = document.getElementById("thatchedroofhouseimg_quant");
-thatchedroofhouseimg_quant.textContent = jsthatchedroofhouseimg_quant;
-
-let jssleepingkittenimg_quant = 10;
-let sleepingkittenimg_quant = document.getElementById("sleepingkittenimg_quant");
-sleepingkittenimg_quant.textContent = jssleepingkittenimg_quant;
-
-let jspaintedhorseimg_quant = 10;
-let paintedhorseimg_quant = document.getElementById("paintedhorseimg_quant");
-paintedhorseimg_quant.textContent = jspaintedhorseimg_quant;
-
-let jssketchedhorseimg_quant = 10;
-let sketchedhorseimg_quant = document.getElementById("sketchedhorseimg_quant");
-sketchedhorseimg_quant.textContent = jssketchedhorseimg_quant;
-
-let jsmouseonbikeimg_quant = 10;
-let mouseonbikeimg_quant = document.getElementById("mouseonbikeimg_quant");
-mouseonbikeimg_quant.textContent = jsmouseonbikeimg_quant;
-
-let jsmousewithglassesimg_quant = 10;
-let mousewithglassesimg_quant = document.getElementById("mousewithglassesimg_quant");
-mousewithglassesimg_quant.textContent = jsmousewithglassesimg_quant;
-
-let jstwowhitehensimg_quant = 10;
-let twowhitehensimg_quant = document.getElementById("twowhitehensimg_quant");
-twowhitehensimg_quant.textContent = jstwowhitehensimg_quant;
-
-let jsfloweryladyimg_quant = 10;
-let floweryladyimg_quant = document.getElementById("floweryladyimg_quant");
-floweryladyimg_quant.textContent = jsfloweryladyimg_quant;
-
-let jsheadbandladyimg_quant = 10;
-let headbandladyimg_quant = document.getElementById("headbandladyimg_quant");
-headbandladyimg_quant.textContent = jsheadbandladyimg_quant;
 
 
