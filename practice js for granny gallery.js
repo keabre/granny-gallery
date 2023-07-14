c0a121ce943caafc3113cb53c99aa309174de0e5
keabre <kieran1907kr@gmail.com>
2023-07-14 15:07:59 +0100

    new changes made to gallery.html and js file
    updated some js regarding prices being shown using js file instead of HTML file

--- practice js for granny gallery.js
+++ practice js for granny gallery.js
@@ -31,7 +31,13 @@ variable names for artwork:
 */
 
 let cockerelimg_price = 7.50;
+const theCockerelPrice = document.querySelector('#cockerel-img-price');
+theCockerelPrice.textContent += cockerelimg_price;
+
 let fruitimg_price = 6.00;
+const theFruitImgPrice = document.querySelector('#fruit-img-price');
+theFruitImgPrice.textContent += fruitimg_price;
+
 let redroofhouseimg_price = 3.00;
 let thatchedroofhouseimg_price = 3.00;
 let housewithwindowimg_price = 60.00;
@@ -48,17 +54,53 @@ let treebylakeimg_price = 75.00;
 let floweryladyimg_price = 6.00;
 let headbandladyimg_price = 6.00;
 
-let cockerelimg_quant = 10;
-let fruitimg_quant = 10;
-let redroofhouseimg_quant = 10;
-let thatchedroofhouseimg_quant = 10;
-let sleepingkittenimg_quant = 10;
-let paintedhorseimg_quant = 10;
-let sketchedhorseimg_quant = 10;
-let mouseonbikeimg_quant = 10;
-let mousewithglassesimg_quant = 10;
-let twowhitehensimg_quant = 10;
-let floweryladyimg_quant = 10;
-let headbandladyimg_quant = 10;
+
+let jscockerelimg_quant = 10;
+let cockerelimg_quant = document.getElementById("cockerelimg_quant");
+cockerelimg_quant.textContent = jscockerelimg_quant;
+
+let jsfruitimg_quant = 10;
+let fruitimg_quant = document.getElementById("fruitimg_quant");
+fruitimg_quant.textContent = jsfruitimg_quant;
+
+let jsredroofhouseimg_quant = 10;
+let redroofhouseimg_quant = document.getElementById("redroofhouseimg_quant");
+redroofhouseimg_quant.textContent = jsredroofhouseimg_quant;
+
+let jsthatchedroofhouseimg_quant = 10;
+let thatchedroofhouseimg_quant = document.getElementById("thatchedroofhouseimg_quant");
+thatchedroofhouseimg_quant.textContent = jsthatchedroofhouseimg_quant;
+
+let jssleepingkittenimg_quant = 10;
+let sleepingkittenimg_quant = document.getElementById("sleepingkittenimg_quant");
+sleepingkittenimg_quant.textContent = jssleepingkittenimg_quant;
+
+let jspaintedhorseimg_quant = 10;
+let paintedhorseimg_quant = document.getElementById("paintedhorseimg_quant");
+paintedhorseimg_quant.textContent = jspaintedhorseimg_quant;
+
+let jssketchedhorseimg_quant = 10;
+let sketchedhorseimg_quant = document.getElementById("sketchedhorseimg_quant");
+sketchedhorseimg_quant.textContent = jssketchedhorseimg_quant;
+
+let jsmouseonbikeimg_quant = 10;
+let mouseonbikeimg_quant = document.getElementById("mouseonbikeimg_quant");
+mouseonbikeimg_quant.textContent = jsmouseonbikeimg_quant;
+
+let jsmousewithglassesimg_quant = 10;
+let mousewithglassesimg_quant = document.getElementById("mousewithglassesimg_quant");
+mousewithglassesimg_quant.textContent = jsmousewithglassesimg_quant;
+
+let jstwowhitehensimg_quant = 10;
+let twowhitehensimg_quant = document.getElementById("twowhitehensimg_quant");
+twowhitehensimg_quant.textContent = jstwowhitehensimg_quant;
+
+let jsfloweryladyimg_quant = 10;
+let floweryladyimg_quant = document.getElementById("floweryladyimg_quant");
+floweryladyimg_quant.textContent = jsfloweryladyimg_quant;
+
+let jsheadbandladyimg_quant = 10;
+let headbandladyimg_quant = document.getElementById("headbandladyimg_quant");
+headbandladyimg_quant.textContent = jsheadbandladyimg_quant;
 
 
